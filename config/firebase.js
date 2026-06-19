28402dd97f808fc1c97e13685a591e7b329ef29f
omar salama <omarsalamaali870@gmail.com>
2026-06-20 00:46:54 +0300

    fix: use base64 for firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,8 @@
 const admin = require('firebase-admin')
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('utf8')
+)
 
 if (!admin.apps.length) {
   admin.initializeApp({
